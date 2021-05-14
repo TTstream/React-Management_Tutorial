d1cc47f2cee0f53971d2e56f0dea929e591a07c8
sk0932 <sk0932@sungkyul.ac.kr>
2021-05-14 11:38:21 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import './App.css';
 import React, { Component } from 'react';
+import Customer from './components/Customer';
 
+const customers= [
+  {
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'옹',
+  'birthday':'969666',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':2  ,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'홍길',
+  'birthday':'321232',
+  'gender':'남자',
+  'job':'프로그래머'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'현아',
+  'birthday':'123',
+  'gender':'남자',
+  'job':'펀드매니저'
+}
+]
 class App extends Component{
   render(){
     return (
-      <div className="App">
-        Hello, React
+      <div>
+        {
+          customers.map(c=>{
+            return(
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            );
+          })
+        }
       </div>
     );
   }
